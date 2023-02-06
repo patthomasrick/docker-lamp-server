d5731461c6c1b54d7a6056298e6cad4d3bad49aa
Patrick Thomas <patrick@patrickwthomas.net>
2023-02-05 19:10:01 -0500

    Formatting, PHP 8, change some ports, add Adminer

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,2 @@
-FROM php:7.4.3-apache
+FROM php:8-apache
 RUN docker-php-ext-install mysqli pdo pdo_mysql
-
--- html/index.php
+++ html/index.php
@@ -1,13 +1,20 @@
 <?php
-$host = "mysql-server";
-$user = "root";
-$pass = "secret";
-$db = "app1";
+
+const DB_HOST = "db";
+const DB_USER = "root";
+const DB_PASS = "secret";
+const DB_DATABASE = "app1";
+
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
+    $conn = new \PDO(
+        // "mysql:host=$host;dbname=$db"
+        sprintf("mysql:host=%s;dbname=%s", DB_HOST, DB_DATABASE),
+        DB_USER,
+        DB_PASS
+    );
+    $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
     echo "Connected successfully";
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
